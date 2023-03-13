d1b0ea0182a02b6df5379c560932888013c31b74
hermana <aeherman@ualberta.ca>
2023-03-12 20:42:56 -0600

    update allowed hosts

--- bigday/settings.py
+++ bigday/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'u7!-y4k1c6b44q507nr_l+c^12o7ur++cpzyn!$65w^!gum@h%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'gettingfiggywithit.dev',
+    'www.gettingfiggywithit.dev'
+    
+]
 
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
